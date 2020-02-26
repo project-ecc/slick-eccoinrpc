a170d6748c8dd0ae3b09321301be241ca39d2dd6
Henry Young <henry@home-young.me.uk>
2020-02-26 19:19:31 +0000

    Migration bitcoin -> eccoin

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 from distutils.core import setup
 
 VERSION = "0.1.4"
-setup(name='slick-bitcoinrpc',
+setup(name='slick-eccoinrpc',
     version=VERSION,
-    description='Just another Python bitcoin-rpc client',
+    description='Just another Python eccoin-rpc client',
     long_description=open('README.rst').read(),
-    url="https://github.com/barjomet/slick-bitcoinrpc",
+    url="https://github.com/project-ecc/slick-eccoinrpc",
     license="MIT",
     author = "Oleksii Ivanchuk",
     author_email = "barjomet@barjomet.com",
-    keywords='bitcoin',
+    keywords='eccoin',
     packages=['slickrpc'],
     install_requires=['configobj',
                       'pycurl',
